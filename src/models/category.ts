e893c364b6e0054cda90b17210500875ba47ba8a
Matheus Farias <matheusaf.contato@gmail.com>
2025-04-29 14:54:13 -0300

    Props

---
+++ src/models/category.ts
@@ -0,0 +1,4 @@
+export type CategoyDTO = {
+    id: number;
+    name: string;
+};
--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -3,6 +3,29 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import { ProductDTO } from '../../models/product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Tv de LED",
+    imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/9b5846b1157b296c3012f16c817f70ad1929fa6f/backend/img/2-big.jpg?raw=true",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
 
 export default function ProductDetails() {
     return (
@@ -10,7 +33,7 @@ export default function ProductDetails() {
             <HeaderClient />
             <main>
                 <section id="product-details-section" className="dsc-container">
-                    <ProductDetailsCard />
+                    <ProductDetailsCard product ={product} />
                     <div className="dsc-btn-page-container">
                         <ButtonPrimary />
                         <ButtonInverse />
