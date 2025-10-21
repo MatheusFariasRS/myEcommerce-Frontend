c02ef0d46aae90222d4dc07614a76316626bb171
Matheus Farias <matheusaf.contato@gmail.com>
2025-10-21 15:28:54 -0300

    Adding React Router Dom

---
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -0,0 +1,52 @@
+import './styles.css';
+import SearchBar from '../../../components/SearchBar';
+import CatalogCard from '../../../components/CatalogCard';
+import ButtonNextPage from '../../../components/ButtonNextPage';
+import { ProductDTO } from '../../../models/product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Tv de LED",
+    imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/9b5846b1157b296c3012f16c817f70ad1929fa6f/backend/img/2-big.jpg?raw=true",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
+
+export default function Catalog() {
+    return (
+        <main>
+            <section id="catalog-section" className="dsc-container">
+                <SearchBar />
+                <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+                    <CatalogCard product={product} />
+
+                </div>
+                <ButtonNextPage />
+            </section>
+        </main>
+    );
+}
---
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -0,0 +1,42 @@
+import './styles.css';
+import ButtonInverse from "../../../components/ButtonInverse";
+import ButtonPrimary from "../../../components/ButtonPrimary";
+import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import { ProductDTO } from '../../../models/product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "Smart TV",
+    description: "Tv de LED",
+    imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/9b5846b1157b296c3012f16c817f70ad1929fa6f/backend/img/2-big.jpg?raw=true",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
+        }
+    ]
+}
+
+
+export default function ProductDetails() {
+    return (
+        <main>
+            <section id="product-details-section" className="dsc-container">
+                <ProductDetailsCard product={product} />
+                <div className="dsc-btn-page-container">
+                    <ButtonPrimary text="Comprar" />
+                    <ButtonInverse text="Início" />
+                </div>
+            </section>
+        </main>
+    );
+}
