3e4b19e6bda8a017ba9eb0b18301a3618fd57004
Matheus Farias <matheusaf.contato@gmail.com>
2025-12-19 17:32:37 -0300

    First back end requests

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,32 +1,37 @@
-import './styles.css';
+import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import * as productService from "../../../services/product-service";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
+  const params = useParams();
 
-    const params = useParams();
+  const [product, setProduct] = useState<ProductDTO>();
 
-    const product = productService.findById(Number(params.productId));
+  useEffect(() => {
+    productService.findById(Number(params.productId)).then((response) => {
+      setProduct(response.data);
+      console.log(response);
+    });
+  }, [params.productId]);
 
-    return (
-        <main>
-            <section id="product-details-section" className="dsc-container">
-                {
-                    product &&
-                    <ProductDetailsCard product={product} />
-                }
+  return (
+    <main>
+      <section id="product-details-section" className="dsc-container">
+        {product && <ProductDetailsCard product={product} />}
 
-                <div className="dsc-btn-page-container">
-                    <ButtonPrimary text="Comprar" />
-                    <Link to="/">
-                        <ButtonInverse text="Início" />
-                    </Link>
-                </div>
-            </section>
-        </main>
-    );
+        <div className="dsc-btn-page-container">
+          <ButtonPrimary text="Comprar" />
+          <Link to="/">
+            <ButtonInverse text="Início" />
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
