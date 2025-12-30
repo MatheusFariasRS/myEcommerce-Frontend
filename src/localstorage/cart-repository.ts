36bb6f811763da63315e59d4bc9eb351be3c8c01
Matheus Farias <matheusaf.contato@gmail.com>
2025-12-30 08:57:35 -0300

    Cart save/get

---
+++ src/localstorage/cart-repository.ts
@@ -0,0 +1,12 @@
+import { OrderDTO } from "../models/order";
+
+export function save(cart: OrderDTO) {
+  const str = JSON.stringify(cart);
+  localStorage.setItem("com.devsuperior.dscomerce/Cart", str);
+}
+
+export function get(): OrderDTO {
+  const str =
+    localStorage.getItem("com.devsuperior.dscomerce/Cart") || '{"items"=[]}';
+  return JSON.parse(str);
+}
